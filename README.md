b370baa9e262485abc969f5fc566ddabc0d779ab
lukisno.kaharman <lukisno.kaharman@1rstwap.com>
2015-08-31 16:02:55 +0700

    Add LICENCE and modify README.md

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# andropad
-Using your Android device as a game pad
+# AndroPAD Server
+
+Using your Android device as a gamepad!
+
+AndroPAD Server is a server application that receive command from Android .&trade; application AndroPAD.
--- andropad.c
+++ andropad.c
@@ -1,14 +1,18 @@
 /*****************************************************************************************************************************************************
  *
  * @file        andropad.c
- * @author      kaharman
+ * @author      Lukisno Kaharman
  * @date        Aug 27, 2015
  *
  * @brief       Andropad main file
  *
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this file,
- * You can obtain one at http://mozilla.org/MPL/2.0/.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  *
  ****************************************************************************************************************************************************/
 
